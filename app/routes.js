f7ba0225774fd3d05a0c00a18f27f9fc164fbb43
Ros <46716815+RosBoyes@users.noreply.github.com>
2022-02-18 14:45:38 +0000

    Save and return
    Started working on a save and return journey but priority is getting new journey for applications.
    
    Will return to this to flesh out.

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
 
 // Add your routes here - above the module.exports line
+
+  // Run this code when a form is submitted to 'account or number'
+  router.post('/save-and-return-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'account or number'
+    var numberAccount = req.session.data['number-or-account']
+
+    // Check whether the variable matches a condition
+    if (numberAccount == "number"){
+      // Send user to next page
+      res.redirect('/number')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/account')
+    }
+
+  })
 
 module.exports = router
